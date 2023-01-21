a1d09c65443caabc25479e5e2a7bb0b3ab6b35e2
Rebjai <15723342+Rebjai@users.noreply.github.com>
2023-01-21 14:57:05 -0600

    added user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { StaffModule } from './staff/staff.module';
 import { ConsumptionSheetsModule } from './consumption-sheets/consumption-sheets.module';
 import { PatientsModule } from './patients/patients.module';
 import { ConsumptionDetailsModule } from './consumption-details/consumption-details.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [RoomsModule, ProductsModule, StaffModule, ConsumptionSheetsModule, PatientsModule, ConsumptionDetailsModule],
+  imports: [RoomsModule, ProductsModule, StaffModule, ConsumptionSheetsModule, PatientsModule, ConsumptionDetailsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
