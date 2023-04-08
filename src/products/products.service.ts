37db4e0484fc97b16354935d072813a59a73f1cf
Rebjai <15723342+Rebjai@users.noreply.github.com>
2023-04-08 05:08:53 -0600

    product service documentation comments

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,10 +10,20 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 import { IPaginationOptions, paginate, Pagination } from 'nestjs-typeorm-paginate';
 
+/**
+ * ProductsService is responsible for handling all the CRUD operations for products.
+ */
 @Injectable()
 export class ProductsService {
   constructor(@InjectRepository(Product) private productsRepository: Repository<Product>) { }
-  async create(createProductDto: CreateProductDto) {
+
+  /**
+   * Creates a new product with the provided details.
+   * @param createProductDto DTO containing the details of the product to be created.
+   * @returns The newly created product.
+   * @throws BadRequestException if the DTO fails validation.
+   */
+  async create(createProductDto: CreateProductDto): Promise<Product> {
     const errors = await validate(plainToInstance(CreateProductDto, createProductDto))
     if (errors.length > 0) {
       throw new BadRequestException();
@@ -25,6 +35,12 @@ export class ProductsService {
     return await this.productsRepository.save(product);
   }
 
+  /**
+   * Finds all products based on the provided search parameters.
+   * @param productQuery DTO containing the search parameters.
+   * @param pagination DTO containing pagination parameters.
+   * @returns A paginated list of products based on the search parameters.
+   */
   async findAll(productQuery?: ProductQueryDto, pagination: PaginationDto = new PaginationDto()): Promise<Pagination<Product>> {
     if (productQuery) {
       const options: FindManyOptions<Product> = {}
@@ -37,10 +53,16 @@ export class ProductsService {
       const response: Pagination<Product> = await paginate<Product>(this.productsRepository, pagination, options)
       return response
     }
-    return paginate<Product>(this.productsRepository, pagination, {order:{name:'DESC',id:'DESC'}})
+    return paginate<Product>(this.productsRepository, pagination, { order: { name: 'DESC', id: 'DESC' } })
   }
 
-  async findOne(id: number) {
+  /**
+   * Finds a product with the provided ID.
+   * @param id The ID of the product to find.
+   * @returns The product with the provided ID.
+   * @throws NotFoundException if the product with the provided ID is not found.
+   */
+  async findOne(id: number): Promise<Product> {
     const product = await this.productsRepository.findOneBy({ id });
     if (!product) {
       throw new NotFoundException();
@@ -48,7 +70,15 @@ export class ProductsService {
     return product
   }
 
-  async update(id: number, updateProductDto: UpdateProductDto) {
+  /**
+   * Updates a product with the provided ID and new details.
+   * @param id The ID of the product to update.
+   * @param updateProductDto DTO containing the new details of the product.
+   * @returns The updated product.
+   * @throws NotFoundException if the product with the provided ID is not found.
+   * @throws BadRequestException if the DTO fails validation.
+   */
+  async update(id: number, updateProductDto: UpdateProductDto): Promise<Product> {
     const product: Product = await this.productsRepository.findOneBy({ id })
     if (!product) {
       throw new NotFoundException();
@@ -62,6 +92,12 @@ export class ProductsService {
     product.category_id = updateProductDto.category_id
     return this.productsRepository.save(product);
   }
+
+  /**
+   * Deletes a product with the provided ID.
+   * @param id The ID of the product to delete.
+   * @returns The result of the delete operation.
+   */
 
   async remove(id: number) {
     return this.productsRepository.softDelete({ id });
