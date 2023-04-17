cec22e91f433d5bd31cfc40ded21a09c868dc4f4
Rebjai <15723342+Rebjai@users.noreply.github.com>
2023-04-17 06:08:32 -0600

    changed name for migration file

--- src/common/db/migrations/1681643372429-AddedUserFieldToConsumptionSheetMigration.ts
+++ src/common/db/migrations/1681643372429-AddedUserFieldToConsumptionSheetMigration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddedUserFieldToConsumptionDetailMigration1681643372429 implements MigrationInterface {
-    name = 'AddedUserFieldToConsumptionDetailMigration1681643372429'
+export class AddedUserFieldToConsumptionSheetMigration1681643372429 implements MigrationInterface {
+    name = 'AddedUserFieldToConsumptionSheetMigration1681643372429'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "consumption_sheet" ADD "total" numeric(10,2)`);
