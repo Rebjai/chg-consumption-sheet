fb714663d14dd71d57ea476b2960974d009b6597
rebjai <15723342+Rebjai@users.noreply.github.com>
2023-08-28 03:25:14 -0600

    using dbhost correctly

--- ormconfig.ts
+++ ormconfig.ts
@@ -29,7 +29,7 @@ const ormAsyncConfig: TypeOrmModuleAsyncOptions = {
     inject: [ConfigService],
     useFactory: (configService: ConfigService) => ({
         type: configService.get<DatabaseType>('DB_DRIVER') as "postgres" | "mysql" | "mariadb",
-        host: 'localhost',
+        host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USER'),
         password: configService.get<string>('DB_PASSWORD'),
